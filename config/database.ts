0c0d932bc2510fb5b4a96a6c05f356363dfd82c2
karuruRaju <raju01102003@gmail.com>
2025-05-30 19:16:17 +0000

    Switched from H2 to SQLite for deployment

--- config/database.ts
+++ config/database.ts
@@ -1,26 +1,9 @@
-import { DataSource } from 'typeorm';
-import { Contact } from '../models/contactModel';
+import { Sequelize } from "sequelize";
 
-export const AppDataSource = new DataSource({
-  type: 'h2',
-  database: 'bitespeeddb',
-  // For H2 in-memory mode, use below URL instead of file db
-  // url: 'jdbc:h2:mem:bitespeeddb;DB_CLOSE_DELAY=-1',
-  synchronize: true,
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
   logging: false,
-  entities: [Contact],
-  // If you want file-based H2 db, you might need this config depending on setup
-  // extra: {
-  //   // H2 specific options here
-  // },
 });
 
-export const initializeDatabase = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log('H2 Database connected successfully.');
-  } catch (error) {
-    console.error('Error during H2 Database initialization', error);
-    throw error;
-  }
-};
+export default sequelize;
